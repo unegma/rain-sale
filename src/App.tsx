5f8b829117c5f8c8f20fef12294ac38c3e1c231b
Timothy Coleman <timhc22@gmail.com>
2022-05-31 21:20:27 +0100

    Add Sale deploy code

--- src/App.tsx
+++ src/App.tsx
@@ -4,60 +4,71 @@ import { OrbitControls, Environment, useGLTF } from '@react-three/drei'
 import NavBar from './components/NavBar';
 // import Modal from './components/Modal';
 import Button from "@mui/material/Button";
-import {ethers} from "ethers";
+import {ethers, Signer} from "ethers";
 import * as rainSDK from "rain-sdk";
-import LocalActivityIcon from '@mui/icons-material/LocalActivity';
-import {Divider, Icon} from "@mui/material";
 import Shoes from "./components/Shoes";
 import { connect } from "./connect.js"; // a very basic web3 connection implementation
 import { opcodeData } from "./opcodeData.js";
 import Typography from "@mui/material/Typography"; // opcode data for RainVM
-
-
 import Box from '@mui/material/Box';
-import FilledInput from '@mui/material/FilledInput';
 import FormControl from '@mui/material/FormControl';
 import FormHelperText from '@mui/material/FormHelperText';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
-import OutlinedInput from '@mui/material/OutlinedInput';
 
 declare var process : {
   env: {
     REACT_APP_RESERVE_TOKEN_ADDRESS: string
     REACT_APP_RESERVE_ERC20_DECIMALS: string
     REACT_APP_REDEEMABLE_ERC20_DECIMALS: string
+    REACT_APP_REDEEMABLE_WALLET_CAP: string
     REACT_APP_STATIC_RESERVE_PRICE_OF_REDEEMABLE: string
     REACT_APP_SALE_TIMEOUT_IN_BLOCKS: string
   }
 }
 
-
 /**
  * App
- *
- * @constructor
  */
 function App() {
-  // const [modalOpen, setModalOpen] = useState(false)
-  // const [selectedImage, setSelectedImage] = useState("")
-  // const [entryAllowed, setEntryAllowed] = useState(false);
-  // const [userBalance, setUserBalance] = useState(0);
-
-  const [signer, setSigner] = useState();
+  const [deploying, setDeploying] = useState(false);
+  const [signer, setSigner] = useState<Signer|undefined>(undefined);
   const [address, setAddress] = useState("");
   const [reserveTokenAddress, setReserveTokenAddress] = useState(process.env.REACT_APP_RESERVE_TOKEN_ADDRESS);
   const [reserveDecimals, setReserveDecimals] = useState(process.env.REACT_APP_RESERVE_ERC20_DECIMALS);
   const [redeemableDecimals, setRedeemableDecimals ] = useState(process.env.REACT_APP_REDEEMABLE_ERC20_DECIMALS);
-  const [redeemableWalletCap, setRedeemableWalletCap] = useState(ethers.constants.MaxUint256);
-  const [staticReservePriceOfRedeemable, setStaticReservePriceOfRedeemable] =
-    useState(ethers.utils.parseUnits(process.env.REACT_APP_STATIC_RESERVE_PRICE_OF_REDEEMABLE, parseInt(reserveDecimals)));
+  const [redeemableWalletCap, setRedeemableWalletCap] = useState(process.env.REACT_APP_REDEEMABLE_WALLET_CAP);
+  const [staticReservePriceOfRedeemable, setStaticReservePriceOfRedeemable] = useState(process.env.REACT_APP_STATIC_RESERVE_PRICE_OF_REDEEMABLE);
   const [saleTimeoutInBlocks, setSaleTimeoutInBlocks] = useState(process.env.REACT_APP_SALE_TIMEOUT_IN_BLOCKS);
+  const [saleView, setSaleView] = React.useState(false); // show sale or admin view (if there is a sale address in the url)
+
+  // run once on render and check url parameters
+  useEffect(() => {
+    let queryString = new URLSearchParams(window.location.search);
+    let saleAddress = queryString.get('s');
+
+    if (typeof saleAddress !== 'undefined' && saleAddress) {
+      console.log(`saleAddress is ${saleAddress}`) // why logged twice: https://stackoverflow.com/questions/60971185/why-does-create-react-app-initialize-twice
+      setSaleView(true);
+    }
+  },[]);
+
+  // basic connection to web3 wallet
+  useEffect(() => {
+    makeWeb3Connection();
+  },[]);
+
+  const amountOfShoes = 2;
+
+  if (!saleView) {
+  }
 
   const handleChangeReserveTokenAddress = (event: React.ChangeEvent<HTMLInputElement>) => {
     setReserveTokenAddress(event.target.value);
   };
-
+  const handleChangeStaticReservePriceOfRedeemable = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStaticReservePriceOfRedeemable(event.target.value);
+  };
 
   async function makeWeb3Connection() {
     try {
@@ -69,96 +80,68 @@ function App() {
     }
   }
 
-  /**
-   * Get UserBalance
-   */
-  // async function getUserBalance() {
-  //   try {
-  //     // const signer = await getSigner();
-  //     // const address = await signer.getAddress();
-  //
-  //     // const gatedNFTContract = new rainSDK.GatedNFT(YOUR_GATEDNFT_ADDRESS, signer);
-  //     // const userBalance = await gatedNFTContract.balanceOf(address);
-  //     // const balance = userBalance.toNumber();
-  //     console.log(`Your Balance: `, balance);
-  //     setUserBalance(balance);
-  //   } catch(err) {
-  //     console.log('------------------------------');
-  //     console.log(`Info: Something went wrong:`, err);
-  //   }
-  // }
 
-  /**
-   * Try to Enter the Gallery
-   *
-   * @param userBalance
-   * @param setEntryAllowed
-   */
-  // async function tryEntry(userBalance: number, setEntryAllowed: any) {
-  //   if (userBalance < 1) {
-  //     console.log('here')
-  //     alert('You need to buy a ticket to enter');
-  //   } else {
-  //     setEntryAllowed(true);
-  //   }
-  // }
+  async function deploySale() {
+    setDeploying(true);
 
-  /**
-   * Purchase Ticket
-   */
-  // async function purchaseTicket(userBalance: number) {
-  //   try {
-  //     if (userBalance >= 1) {
-  //       alert('You already have a ticket!');
-  //     } else {
-  //       const signer = await getSigner();
-  //       const address = await signer.getAddress();
-  //
-  //       alert('Minting, please wait a moment and don\`t refresh the page!');
-  //       const gatedNFTContract = new rainSDK.GatedNFT(YOUR_GATEDNFT_ADDRESS, signer);
-  //       await gatedNFTContract.mint(address); // get one of the NFTs needed to take part in the sale
-  //
-  //       // todo add a way to check when the mint has finished
-  //
-  //       await getUserBalance(); // this will trigger useEffect and update the text in the frontend
-  //       alert('Done, you will (currently) need to refresh the page when the mint is complete!');
-  //     }
-  //   } catch(err) {
-  //     console.log('------------------------------');
-  //     console.log(`Info: Something went wrong:`, err);
-  //   }
-  // }
-
-  /**
-   * Get Balance on Startup
-   */
-  // useEffect(() => {
-  //   getUserBalance()
-  // }, [userBalance])
-
-  const [saleView, setSaleView] = React.useState(false);
-
-  // run once on render
-  useEffect(() => {
-    let queryString = new URLSearchParams(window.location.search);
-    let saleAddress = queryString.get('s');
-
-    if (typeof saleAddress !== 'undefined' && saleAddress) {
-      console.log(`saleAddress is ${saleAddress}`) // why logged twice: https://stackoverflow.com/questions/60971185/why-does-create-react-app-initialize-twice
-      setSaleView(true);
+    const saleConfig = {
+      canStartStateConfig: opcodeData.canStartStateConfig, // config for the start of the Sale (see opcodes section below)
+      canEndStateConfig: opcodeData.canEndStateConfig, // config for the end of the Sale (see opcodes section below)
+      calculatePriceStateConfig: opcodeData.calculatePriceStateConfig(
+        ethers.utils.parseUnits(staticReservePriceOfRedeemable, parseInt(reserveDecimals)),
+        ethers.utils.parseUnits(redeemableWalletCap, parseInt(redeemableDecimals)),
+      ), // config for the `calculatePrice` function (see opcodes section below).
+      recipient: address, // who will receive the RESERVE token (e.g. USDCC) after the Sale completes
+      reserve: reserveTokenAddress, // the reserve token contract address (MUMBAI MATIC in this case)
+      saleTimeout: saleTimeoutInBlocks,
+      cooldownDuration: 100, // this will be 100 blocks (10 mins on MUMBAI) // this will stay as blocks in upcoming releases
+      minimumRaise: ethers.utils.parseUnits("10", reserveDecimals), // minimum to complete a Raise // TODO CHECK IF FINISHES AUTOMATICALLY WHEN HIT
+      dustSize: ethers.utils.parseUnits("0", reserveDecimals), // todo check this: for bonding curve price curves (that generate a few left in the contract at the end)
+    };
+    const redeemableConfig = {
+      // todo can erc721 be used instead?
+      erc20Config: { // config for the redeemable token (rTKN) which participants will get in exchange for reserve tokens
+        name: "Shoes", // the name of the rTKN
+        symbol: "shoeNFT", // the symbol for your rTKN
+        distributor: "0x0000000000000000000000000000000000000000", // distributor address
+        initialSupply: ethers.utils.parseUnits("100", redeemableDecimals), // initial rTKN supply
+      },
+      // todo why can't I decompile? https://mumbai.polygonscan.com/address/0xC064055DFf6De32f44bB7cCB0ca59Cbd8434B2de#code
+      tier: "0xC064055DFf6De32f44bB7cCB0ca59Cbd8434B2de", // tier contract address (used for gating)
+      minimumTier: 0, // minimum tier a user needs to take part
+      distributionEndForwardingAddress: "0x0000000000000000000000000000000000000000" // the rTKNs that are not sold get forwarded here (0x00.. will burn them)
     }
-  },[]);
 
-  useEffect(() => {
-    makeWeb3Connection();
-  },[]);
+    // todo get ratios of costs
+    // todo what happens if one fails (inform users)
+    console.warn("Info: It is important to let your users know how many transactions to expect and what they are. " +
+      "This example consists of 5 Transactions:\n\n" +
+      "* Create Sale (For Admins) (fee+gas cost at circa 2022-05-30T15:32:44Z: 0.002108 MATIC)\n" + // todo check how much gas costs can fluctuate (gas cost at 2022-05-30T15:27:32Z: 0.001992 MATIC) (gas cost at 2022-05-30T15:32:44Z: 0.044359 MATIC)
+      "* Start Sale (For Admins) (fee+gas cost at circa 2022-05-30T15:32:44Z: 0.000061 MATIC) \n" +
+      // todo what is this contract address? and is it approved to spend this again in future or only up to this amount?
+      "* Give Permission to 0x642d4e6d828436ee95658c3462b46dafc1d0a61a to access USDCC (For Users) (fee+gas at circa 2022-05-30T15:32:44Z: 0.00009 MATIC) \n" +
+      "* Buying from Sale (For Users) (fee+gas cost at circa 2022-05-30T15:32:44Z: 0.000531 MATIC) \n" +
+      "* End Sale (For Admins) (fee+gas at circa 2022-05-30T15:32:44Z: 0.000158 MATIC) \n"
+    );
+    console.log('------------------------------'); // separator
 
-  const amountOfShoes = 2;
+    console.log("Info: Creating Sale with the following state:", saleConfig, redeemableConfig);
+    // @ts-ignore
+    const saleContract = await rainSDK.Sale.deploy(signer, saleConfig, redeemableConfig);
+    console.log('Result: Sale Contract:', saleContract); // the Sale contract and corresponding address
+    const redeemableContract = await saleContract.getRedeemable();
+    console.log('Result: Redeemable Contract:', redeemableContract); // the Sale contract and corresponding address
+    console.log('------------------------------'); // separator
 
+    // ### Interact with the newly deployed ecosystem
 
+    console.log('Info: Starting The Sale.');
+    const startStatusTransaction = await saleContract.start();
+    const startStatusReceipt = await startStatusTransaction.wait();
+    console.log('Info: Sale Started Receipt:', startStatusReceipt);
+    console.log('------------------------------'); // separator
 
-  if (!saleView) {
-
+    setDeploying(false);
   }
 
 
@@ -168,28 +151,6 @@ function App() {
   return (
     <div className="rootContainer">
       <NavBar />
-
-
-
-      {/*<div hidden={entryAllowed} className="gatedSection">*/}
-      {/*  <div className="gatedSection__left"></div>*/}
-      {/*  <div className="gatedSection__right">*/}
-      {/*    <div className="gatedSection__info">*/}
-      {/*      <h1>Welcome to MetaGallery</h1><p><span>Please either show or purchase an entry ticket to enter. <br/><br/>*/}
-      {/*      You will need to connect to Polygon Mumbai Testnet, and have some Testnet Matic. <br/><br/>*/}
-      {/*      You might also want to check the console in your browser (works best in Chrome).</span></p>*/}
-
-      {/*      <br/>*/}
-      {/*      <Divider color="white" variant="fullWidth"/>*/}
-      {/*      <br/>*/}
-
-      {/*      <p>You currently have {userBalance < 1 ? "no ticket!" : "a ticket!"}</p><div className="ticket" hidden={userBalance == 0}><LocalActivityIcon/></div>*/}
-      {/*      <br/>*/}
-      {/*      <Button onClick={() => {tryEntry(userBalance, setEntryAllowed)}} variant="outlined" color="primary">Enter</Button>&nbsp;*/}
-      {/*      <Button onClick={() => {purchaseTicket(userBalance)}} variant="outlined" color="secondary">Purchase</Button>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
 
       { !saleView && (
         <>
@@ -220,7 +181,20 @@ function App() {
               </FormHelperText>
             </FormControl>
 
-            <Button variant="outlined">Deploy Sale</Button>
+            <FormControl variant="standard">
+              <InputLabel htmlFor="component-helper">The Price of a Shoe NFT</InputLabel>
+              <Input
+                id="component-helper"
+                value={staticReservePriceOfRedeemable}
+                onChange={handleChangeStaticReservePriceOfRedeemable}
+                aria-describedby="component-helper-text"
+              />
+              <FormHelperText id="component-helper-text">
+                Default: 1 of the above Tokens
+              </FormHelperText>
+            </FormControl>
+
+            <Button disabled={deploying} variant="outlined" onClick={() => {deploySale()}}>Deploy Sale</Button>
 
             {/*<FormControl disabled variant="standard">*/}
             {/*  <InputLabel htmlFor="component-disabled">Name</InputLabel>*/}
