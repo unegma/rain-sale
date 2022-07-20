508a4ae60d32c338f27f4e09c646b79bda024d03
Timothy Coleman <timhc22@gmail.com>
2022-07-20 18:11:56 +0100

    Fix label issue

--- src/components/Web3ConnectionButtons.tsx
+++ src/components/Web3ConnectionButtons.tsx
@@ -114,9 +114,10 @@ export default function Web3ConnectionButtons({setAddress, setSettingsOpen}: any
               const connected = currentConnector === connector;
               const disabled = !triedEager || !!activatingConnector || connected || !!error;
 
+              let nameLabel;
               // bit of a hacky override
               if (name === 'Metamask') {
-                name = 'Metamask/Browser'
+                nameLabel = 'Metamask/Browser'
               }
 
               return (
@@ -141,7 +142,7 @@ export default function Web3ConnectionButtons({setAddress, setSettingsOpen}: any
                         </span>
                       )}
                     </div>
-                    {name}
+                    {nameLabel}
                   </Button>
                 )
               )
