76bd7e1a35516be712e1776cf12e7043cbdfb3d5
Timothy Coleman <timhc22@gmail.com>
2022-07-21 18:40:01 +0100

    Remove unused env

--- src/components/various/Console.tsx
+++ src/components/various/Console.tsx
@@ -1,6 +1,5 @@
 import Typography from "@mui/material/Typography";
 import React from "react";
-const CHAIN_NAME = process.env.REACT_APP_CHAIN_NAME; // Mumbai (Polygon Testnet) Chain ID
 
 export default function Console({consoleColor, consoleData}: any) {
   return (
