3fd4d33df38652e3cfce9d04e78d5cbd0bf10a41
Timothy Coleman <timhc22@gmail.com>
2022-07-22 13:50:26 +0100

    Change console to styled componenet

--- src/components/various/Console.tsx
+++ src/components/various/Console.tsx
@@ -1,16 +1,30 @@
-import Typography from "@mui/material/Typography";
+import styled from 'styled-components';
 import React from "react";
+import Typography from "@mui/material/Typography";
+
+const ConsoleContainer = styled.div`
+  text-align: center;
+  
+  .consoleTextRed {
+    color: red;
+    user-select: text;
+  }
+  .consoleTextGreen {
+    color: green;
+    user-select: text;
+  }
+`;
 
 export default function Console({consoleColor, consoleData}: any) {
   return (
-    <div className='console'>
+    <ConsoleContainer className='console'>
       { consoleColor === 'red' && (
-        <Typography className="modalTextRed"><b>{consoleData}</b></Typography>
+        <Typography className="consoleTextRed"><b>{consoleData}</b></Typography>
       )}
 
       { consoleColor === 'green' && (
-        <Typography className="modalTextGreen"><b>{consoleData}</b></Typography>
+        <Typography className="consoleTextGreen"><b>{consoleData}</b></Typography>
       )}
-    </div>
+    </ConsoleContainer>
   )
 }
