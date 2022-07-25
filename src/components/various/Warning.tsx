fea2941eb90a6b7ff71a5e536f279d8ab3b4945f
Timothy Coleman <timhc22@gmail.com>
2022-07-25 11:02:15 +0100

    Add Space

--- src/components/various/Warning.tsx
+++ src/components/various/Warning.tsx
@@ -5,7 +5,7 @@ const CHAIN_NAME = process.env.REACT_APP_CHAIN_NAME; // Mumbai (Polygon Testnet)
 export default function Warning() {
   return (
     <Typography color="red">
-      Must be connected to <a href={`https://www.youtube.com/watch?v=I4C5RkiNAYQ`} target="_blank"><b className=''>{CHAIN_NAME}</b>&nbsp;Network</a>
+      Must be connected to <a href={`https://www.youtube.com/watch?v=I4C5RkiNAYQ`} target="_blank"><b className=''>{CHAIN_NAME}</b>&nbsp;Network</a>&nbsp;
       (toggle 'Testnets' on <a href={`https://chainlist.org/?search=mumbai&testnets=true`} target="_blank">chainlist.org</a> for details).&nbsp;
       <span className="red"><b>Warning: Proceeding may result in the loss of transaction fees.</b></span>
     </Typography>
